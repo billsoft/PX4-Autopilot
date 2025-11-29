3f395bbf63fd78db7c8e9e4ee0449ee570092f6f
billsoft <billsoft@163.com>
2025-11-29 19:58:59 +0800

    board: switch CMOS EXTI pins to PE3/PE4 per board docs; align SPI3 CS to PA15

--- boards/st/nucleo-h743zi-fc/src/spi.cpp
+++ boards/st/nucleo-h743zi-fc/src/spi.cpp
@@ -7,7 +7,7 @@ const px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
         initSPIDevice(DRV_IMU_DEVTYPE_ICM42688P, SPI::CS{GPIO::PortD, GPIO::Pin14}),
     }),
     initSPIBus(SPI::Bus::SPI3, {  /* Changed from SPI2 to SPI3 to avoid Ethernet RMII conflict */
-        initSPIDevice(DRV_IMU_DEVTYPE_ICM42688P, SPI::CS{GPIO::PortA, GPIO::Pin4}),  /* PA4 = D24 */
+        initSPIDevice(DRV_IMU_DEVTYPE_ICM42688P, SPI::CS{GPIO::PortA, GPIO::Pin15}),  /* PA15 = D20 per board doc */
     }),
 };
 
